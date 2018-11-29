4b5b94703af8718b9060d18bed686d10ca30dee5
Joey Kleingers <joey.kleingers@bluequartz.net>
2018-11-29 14:19:15 -0500

    Creating GenericMontage subfolder.

---
+++ SIMPLVtkLib/Wizards/GenericMontage/SourceList.cmake
@@ -0,0 +1,30 @@
+
+set(${PROJECT_NAME}_GenericMontage_HDRS
+  ${WIZARDS_SOURCE_DIR}/GenericMontage/GenericMontageWizard.h
+  ${WIZARDS_SOURCE_DIR}/GenericMontage/GenericCollectionTypePage.h
+)
+
+set(${PROJECT_NAME}_GenericMontage_SRCS
+  ${WIZARDS_SOURCE_DIR}/GenericMontage/GenericMontageWizard.cpp
+  ${WIZARDS_SOURCE_DIR}/GenericMontage/GenericCollectionTypePage.cpp
+)
+
+set(${PROJECT_NAME}_GenericMontage_UIS
+  ${WIZARDS_SOURCE_DIR}/GenericMontage/UI_Files/GenericCollectionTypePage.ui
+)
+
+cmp_IDE_SOURCE_PROPERTIES( "${PROJECT_NAME}/Wizards" "${${PROJECT_NAME}_GenericMontage_HDRS}" "${${PROJECT_NAME}_GenericMontage_SRCS}" "0")
+
+# Organize the Source files for things like Visual Studio and Xcode
+cmp_IDE_GENERATED_PROPERTIES("${WIZARDS_SOURCE_DIR}/GenericMontage/UI_Files" "${${PROJECT_NAME}_GenericMontage_UIS}" "")
+
+# --------------------------------------------------------------------
+# -- Run UIC on the necessary files
+QT5_WRAP_UI( ${PROJECT_NAME}_GenericMontage_Generated_UI_HDRS ${${PROJECT_NAME}_GenericMontage_UIS} )
+foreach(h ${${PROJECT_NAME}_GenericMontage_Generated_UI_HDRS})
+  set_property(SOURCE ${h} PROPERTY SKIP_AUTOMOC ON)
+endforeach()
+
+cmp_IDE_SOURCE_PROPERTIES( "Generated/Qt_Uic" "${${PROJECT_NAME}_GenericMontage_Generated_UI_HDRS}" "" "0")
+
+include_directories(${WIZARDS_SOURCE_DIR}/GenericMontage/UI_Files)
--- SIMPLVtkLib/Wizards/SourceList.cmake
+++ SIMPLVtkLib/Wizards/SourceList.cmake
@@ -1,28 +1,14 @@
+set(WIZARDS_SOURCE_DIR "${${PROJECT_NAME}_SOURCE_DIR}/SIMPLVtkLib/Wizards")
 
 set(${PROJECT_NAME}_Wizards_HDRS
-
+  ${WIZARDS_SOURCE_DIR}/AbstractMontageWizard.h
 )
 
 set(${PROJECT_NAME}_Wizards_SRCS
-
-)
-
-set(${PROJECT_NAME}_Wizards_UIS
-
+  ${WIZARDS_SOURCE_DIR}/AbstractMontageWizard.cpp
 )
 
 cmp_IDE_SOURCE_PROPERTIES( "${PROJECT_NAME}/Wizards" "${${PROJECT_NAME}_Wizards_HDRS}" "${${PROJECT_NAME}_Wizards_SRCS}" "0")
 
-# Organize the Source files for things like Visual Studio and Xcode
-cmp_IDE_GENERATED_PROPERTIES("${PROJECT_NAME}/Wizards/UI_Files" "${${PROJECT_NAME}_Wizards_UIS}" "")
-
-# --------------------------------------------------------------------
-# -- Run UIC on the necessary files
-QT5_WRAP_UI( ${PROJECT_NAME}_Wizards_Generated_UI_HDRS ${${PROJECT_NAME}_QtWidgets_UIS} )
-foreach(h ${${PROJECT_NAME}_Wizards_Generated_UI_HDRS})
-  set_property(SOURCE ${h} PROPERTY SKIP_AUTOMOC ON)
-endforeach()
-
-cmp_IDE_SOURCE_PROPERTIES( "Generated/Qt_Uic" "${${PROJECT_NAME}_Wizards_Generated_UI_HDRS}" "" "0")
-
-include_directories(${${PROJECT_NAME}_SOURCE_DIR}/SIMPLVtkLib/Wizards/UI_Files)
+  # -- Include the Generic Montage sources
+include(${${PROJECT_NAME}_SOURCE_DIR}/SIMPLVtkLib/Wizards/GenericMontage/SourceList.cmake)
